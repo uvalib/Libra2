7d2e02cfe83b3186fdf68b2a86c28a79229a60a7
dpg3k <dpg3k@d-137-155-231.bootp.Virginia.EDU>
2016-01-14 10:36:13 -0500

    Docker image containing just application; jetty component in another image

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,9 +1,4 @@
-# recycle jetty as appropriate
-#rake jetty:stop
-#rake jetty:start
-
 # start the resque pool daemon
-#RUN_AT_EXIT_HOOKS=true TERM_CHILD=1 resque-pool --daemon --environment development stop
 RUN_AT_EXIT_HOOKS=true TERM_CHILD=1 resque-pool --daemon --environment development start
 
 # run the server
