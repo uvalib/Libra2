902d9ad8a2329967bd698954d5d7c53a43504e33
Sue Richeson <spr7b@virginia.edu>
2016-02-04 15:00:46 -0500

    Add 'Fourth Year Thesis' to list of resource types--one more place.

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -21,6 +21,7 @@ Sufia.config do |config|
     "Conference Proceeding" => "Conference Proceeding",
     "Dataset" => "Dataset",
     "Dissertation" => "Dissertation",
+    "Fourth Year Thesis" => "Fourth Year Thesis",
     "Image" => "Image",
     "Journal" => "Journal",
     "Map or Cartographic Material" => "Map or Cartographic Material",
