fdd69dd33cc00c4812fd9dda2d7feb5dcc4ebe8f
UVADave <dpg3k@virginia.edu>
2016-03-16 15:32:18 -0400

    LIBRA-15: add some of the new metadata fields

--- app/forms/curation_concerns/generic_work_form.rb
+++ app/forms/curation_concerns/generic_work_form.rb
@@ -1,10 +1,39 @@
-# Generated via
-#  `rails generate curation_concerns:work GenericWork`
+
 module CurationConcerns
   class GenericWorkForm < Sufia::Forms::WorkForm
+
     self.model_class = ::GenericWork
+
     include HydraEditor::Form::Permissions
-    self.terms += [:resource_type]
+
+    self.terms = [:title,
+                  :creator,
+                  :contributor,
+                  :description,
+                  :subject,
+                  :language,
+                  :publisher,
+                  :date_created,
+    #              :tag,
+                  :identifier,
+    #              :based_near,
+                  :related_url,
+
+                  :department,
+                  :degree,
+                  :notes,
+                  :sponsoring_agency,
+
+    #              :representative_id, :thumbnail_id, :files,
+    #              :visibility_during_embargo, :embargo_release_date, :visibility_after_embargo,
+    #              :visibility_during_lease, :lease_expiration_date, :visibility_after_lease,
+    #              :visibility,
+    #              :resource_type
+
+                   :rights,
+                   :license
+                  ]
+
 
   end
 end
