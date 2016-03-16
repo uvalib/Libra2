3d5fdb4c808869fe00e1ab8ee0f4d028979c4db1
UVADave <dpg3k@virginia.edu>
2016-03-16 11:24:43 -0400

    LIBRA-84: update the metadata model

--- lib/libra2/app/models/concerns/basic_metadata.rb
+++ lib/libra2/app/models/concerns/basic_metadata.rb
@@ -1,5 +1,7 @@
-module CurationConcerns
+module Libra2
+
   module BasicMetadata
+
     extend ActiveSupport::Concern
 
     included do
@@ -12,29 +14,37 @@ module CurationConcerns
       end
 
       property :part_of, predicate: ::RDF::Vocab::DC.isPartOf
-      property :resource_type, predicate: ::RDF::Vocab::DC.type do |index|
+
+      property :resource_type, predicate: ::RDF::Vocab::DC.type, multiple: false do |index|
         index.as :stored_searchable, :facetable
       end
-      property :creator, predicate: ::RDF::Vocab::DC.creator do |index|
+
+      property :creator, predicate: ::RDF::Vocab::DC.creator, multiple: false do |index|
         index.as :stored_searchable, :facetable
       end
+
       property :contributor, predicate: ::RDF::Vocab::DC.contributor do |index|
         index.as :stored_searchable, :facetable
       end
-      property :description, predicate: ::RDF::Vocab::DC.description do |index|
+
+      property :description, predicate: ::RDF::Vocab::DC.description, multiple: false do |index|
         index.type :text
         index.as :stored_searchable
       end
-      property :tag, predicate: ::RDF::Vocab::DC.relation do |index|
+
+      property :tag, predicate: ::RDF::Vocab::DC.relation, multiple: false do |index|
         index.as :stored_searchable, :facetable
       end
+
       property :rights, predicate: ::RDF::Vocab::DC.rights do |index|
         index.as :stored_searchable
       end
-      property :publisher, predicate: ::RDF::Vocab::DC.publisher do |index|
+
+      property :publisher, predicate: ::RDF::Vocab::DC.publisher, multiple: false do |index|
         index.as :stored_searchable, :facetable
       end
-      property :date_created, predicate: ::RDF::Vocab::DC.created do |index|
+
+      property :date_created, predicate: ::RDF::Vocab::DC.created, multiple: false do |index|
         index.as :stored_searchable
       end
 
@@ -52,24 +62,31 @@ module CurationConcerns
         index.type :date
         index.as :stored_sortable
       end
+
       property :subject, predicate: ::RDF::Vocab::DC.subject do |index|
         index.as :stored_searchable, :facetable
       end
-      property :language, predicate: ::RDF::Vocab::DC.language do |index|
+
+      property :language, predicate: ::RDF::Vocab::DC.language, multiple: false do |index|
         index.as :stored_searchable, :facetable
       end
-      property :identifier, predicate: ::RDF::Vocab::DC.identifier do |index|
+
+      property :identifier, predicate: ::RDF::Vocab::DC.identifier, multiple: false do |index|
         index.as :stored_searchable
       end
-      property :based_near, predicate: ::RDF::Vocab::FOAF.based_near do |index|
+
+      property :based_near, predicate: ::RDF::Vocab::FOAF.based_near, multiple: false do |index|
         index.as :stored_searchable, :facetable
       end
+
       property :related_url, predicate: ::RDF::RDFS.seeAlso do |index|
         index.as :stored_searchable
       end
+
       property :bibliographic_citation, predicate: ::RDF::Vocab::DC.bibliographicCitation do |index|
         index.as :stored_searchable
       end
+
       property :source, predicate: ::RDF::Vocab::DC.source do |index|
         index.as :stored_searchable
       end
