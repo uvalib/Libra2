b158e5c977c0c054b57896526f4d5f0b16ba5dfb
UVADave <dpg3k@virginia.edu>
2016-03-16 15:32:43 -0400

    LIBRA-15: add new metadata fields

--- lib/libra2/app/models/generic_work.rb
+++ lib/libra2/app/models/generic_work.rb
@@ -12,10 +12,10 @@ class GenericWork < ActiveFedora::Base
   WORK_TYPE_THESIS = 'thesis'.freeze
 
   # Custom Metadata
-  #
+
   # work_type - Currently either a 'thesis' or a 'generic_work'.
   property :work_type, predicate: ::RDF::URI('http://example.org/terms/work_type'), multiple: false do |index|
-    index.as :stored_searchable
+    index.as :stored_searchable, :facetable
   end
 
   # draft - Pertinent only for 'thesis' work_type.
@@ -28,12 +28,49 @@ class GenericWork < ActiveFedora::Base
     index.as :stored_searchable
   end
 
+  # the institution name of the author; always UVa for thesis
+  property :author_institution, predicate: ::RDF::URI('http://example.org/terms/institution'), multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  # which school/department (from SIS or the deposit registration process)
+  property :department, predicate: ::RDF::URI('http://example.org/terms/department'), multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  # which degree (from SIS or the deposit registration process)
+  property :degree, predicate: ::RDF::URI('http://example.org/terms/degree'), multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  #
+  property :notes, predicate: ::RDF::URI('http://example.org/terms/notes'), multiple: false do |index|
+    index.type :text
+    index.as :stored_searchable
+  end
+
+  #
+  property :license, predicate: ::RDF::URI('http://example.org/terms/license') do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  #
+  property :sponsoring_agency, predicate: ::RDF::URI('http://example.org/terms/sponsoring_agency'), multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  #
+  property :admin_notes, predicate: ::RDF::URI('http://example.org/terms/admin_notes') do |index|
+    index.type :text
+    index.as :stored_searchable
+  end
+
   class << self
 
     # determine which fields can have multiple values...
     def multiple?( term )
       case term.to_s
-        when 'title', 'contributor', 'subject', 'related_url'
+        when 'title', 'contributor', 'subject', 'related_url', 'admin_notes'
           true
         else
           false
