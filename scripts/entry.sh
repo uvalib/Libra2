170a0f69843490bbc21359d59dbfe8743868ac42
UVADave <dpg3k@virginia.edu>
2016-03-18 10:38:03 -0400

    Start the rescue pool based on the RAILS_ENV

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,5 +1,5 @@
 # start the resque pool daemon
-RUN_AT_EXIT_HOOKS=true TERM_CHILD=1 resque-pool --daemon --environment development start
+RUN_AT_EXIT_HOOKS=true TERM_CHILD=1 resque-pool --daemon --environment $RAILS_ENV  start
 
 # run the server
 rails server -b 0.0.0.0 -p 3000
