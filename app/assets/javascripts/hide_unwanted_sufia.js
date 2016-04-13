832fbbf940d473bd64a8ca8cf3238cc55e1a24e0
Paul Rosen <paul@paulrosen.net>
2016-04-13 16:16:38 -0400

    Fix initialization when turbolinks is present.

--- app/assets/javascripts/hide_unwanted_sufia.js
+++ app/assets/javascripts/hide_unwanted_sufia.js
@@ -1,13 +1,14 @@
-function initPage() {
+(function() {
 	"use strict";
+	function initPage() {
 
-	// Select the activity tab by default in the profile page, because the default tab (Highlight) is now hidden.
-	var activityTab = $('.profile .nav-tabs a[href="#activity_log"]');
-	if (activityTab.length > 0)
-		activityTab.tab('show');
-}
+		// Select the activity tab by default in the profile page, because the default tab (Highlight) is now hidden.
+		var activityTab = $('.profile .nav-tabs a[href="#activity_log"]');
+		if (activityTab.length > 0)
+			activityTab.tab('show');
+	}
 
-$(window).bind('page:change', function() {
-	"use strict";
-	initPage();
-});
+	$(window).bind('page:change', function() {
+		initPage();
+	});
+})();
