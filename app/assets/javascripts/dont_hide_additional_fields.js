cbe29c798b7ef787dd5b20ad9a58f4aa611e9200
Paul Rosen <paul@paulrosen.net>
2016-04-13 15:00:40 -0400

    Always expand the "below the fold" fields.

---
+++ app/assets/javascripts/dont_hide_additional_fields.js
@@ -0,0 +1,11 @@
+$(function() {
+	"use strict";
+
+	// If there is an "Additional Fields" button on the page, then show the extended fields and hide the button.
+	// This is on the "Add New Work" page and the corresponding edit page.
+	var additionalFieldsButton = $('a[data-toggle="collapse"][href="#extended-terms"]');
+	if (additionalFieldsButton.length > 0) {
+		additionalFieldsButton.click();
+		additionalFieldsButton.hide();
+	}
+});
