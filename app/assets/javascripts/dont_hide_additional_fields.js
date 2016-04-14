f4af391e39dd0aaaad1d042da739447d7450fbef
Paul Rosen <paul@paulrosen.net>
2016-04-14 10:20:01 -0400

    Fix initialization when turbolinks is present.

--- app/assets/javascripts/dont_hide_additional_fields.js
+++ app/assets/javascripts/dont_hide_additional_fields.js
@@ -6,8 +6,10 @@
 		// If there is an "Additional Fields" button on the page, then show the extended fields and hide the button.
 		// This is on the "Add New Work" page and the corresponding edit page.
 		var additionalFieldsButton = $('a[data-toggle="collapse"][href="#extended-terms"]');
+		var externalFields = $("#extended-terms");
 		if (additionalFieldsButton.length > 0) {
-			additionalFieldsButton.click();
+			if (!externalFields.hasClass("in")) // This can get called twice sometimes, so we don't want to toggle it twice.
+				additionalFieldsButton.click();
 			additionalFieldsButton.hide();
 		}
 	}
