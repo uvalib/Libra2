355dc993bfdca0ba213ac49a71546806561fe759
Paul Rosen <paul@paulrosen.net>
2016-04-27 15:00:13 -0400

    Added a suggestion for the new work flow. This includes the new routes /preview and /submit and the new preview page.

--- lib/libra2/app/helpers/libra_helper.rb
+++ lib/libra2/app/helpers/libra_helper.rb
@@ -4,4 +4,17 @@ module LibraHelper
     render partial: 'my/theses_list'
   end
 
+  def render_thesis_attribute(label, value)
+    if value.kind_of?(Array)
+      payload = []
+      value.each { |v|
+        payload.push(content_tag(:li, v, { class: "attribute" }))
+      }
+      payload = raw(payload.join("\n"))
+    else
+        payload = content_tag(:li, value, { class: "attribute" })
+    end
+    row = raw(content_tag(:th, label, {}) + content_tag(:td, raw("<ul class=\"tabular\">#{payload}</ul>")))
+    return content_tag(:tr, row, {})
+  end
 end
