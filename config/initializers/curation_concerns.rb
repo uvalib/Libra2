a9be776177072b796ed75d29e37b811b62eb8437
UVADave <dpg3k@virginia.edu>
2016-04-28 14:41:13 -0400

    Correctly specify working path

--- config/initializers/curation_concerns.rb
+++ config/initializers/curation_concerns.rb
@@ -32,7 +32,7 @@ CurationConcerns.configure do |config|
   # Location on local file system where uploaded files will be staged
   # prior to being ingested into the repository or having derivatives generated.
   # If you use a multi-server architecture, this MUST be a shared volume.
-  config.working_path = File.join( Rails.root, 'hostfs', 'uploads', 'derivatives' )
+  config.working_path = File.join( Rails.root, 'hostfs', 'uploads', 'originals' )
 
   # If you have ffmpeg installed and want to transcode audio and video uncomment this line
   # config.enable_ffmpeg = true
