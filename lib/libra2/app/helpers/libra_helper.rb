0a30e8b7bc9ac184806c77e397a33cffd3a45cdf
UVADave <dpg3k@virginia.edu>
2016-05-05 11:46:13 -0400

    LIBRA-148/149: present the depositor with the correct deposit agreement

--- lib/libra2/app/helpers/libra_helper.rb
+++ lib/libra2/app/helpers/libra_helper.rb
@@ -17,4 +17,12 @@ module LibraHelper
     row = raw(content_tag(:th, label, {}) + content_tag(:td, raw("<ul class=\"tabular\">#{payload}</ul>")))
     return content_tag(:tr, row, {})
   end
+
+  def deposit_agreement_url
+    if current_user && current_user.title && current_user.title.match( /^Undergraduate/ )
+      return 'http://www.library.virginia.edu/libra/open-access/libra-deposit-license/#uvaonly'
+    end
+    return 'http://www.library.virginia.edu/libra/etds/etd-license'
+  end
+
 end
