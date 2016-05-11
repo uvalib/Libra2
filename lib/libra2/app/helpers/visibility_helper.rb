d4f5637f4c8f99636c2985d20dbda4f14961bf02
UVADave <dpg3k@virginia.edu>
2016-05-11 15:30:46 -0400

    Show apprioriate visibility options when doing an embargo

--- lib/libra2/app/helpers/visibility_helper.rb
+++ lib/libra2/app/helpers/visibility_helper.rb
@@ -1,22 +1,28 @@
 module VisibilityHelper
 
-  def post_embargo_visibility_options
+  def embargo_visibility_options( options = nil )
     options = [
        Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC,
        Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
-    ]
+    ] if options.nil?
 
     options.map { |value| [ visibility_text(value), value ] }
   end
 
-  def embargo_duration_options
+  def embargo_duration_options( options = nil )
     options = [
         GenericWork::EMBARGO_VALUE_6_MONTH,
         GenericWork::EMBARGO_VALUE_1_YEAR,
         GenericWork::EMBARGO_VALUE_2_YEAR,
         GenericWork::EMBARGO_VALUE_5_YEAR
-    ]
+    ] if options.nil?
+
     options.map { |value| [ duration_text(value), value ] }
+  end
+
+  def edit_link_if_draft(document)
+    return edit_curation_concerns_generic_work_path(document) if document && document.is_draft?
+    return curation_concerns_generic_work_path(document)
   end
 
   private
@@ -29,18 +35,4 @@ module VisibilityHelper
     t("libra.duration.#{value}.text", default: value )
   end
 
-  def is_draft(solr_document)
-    return true if solr_document.nil?
-    return false if solr_document.draft.nil?
-    return true if "#{solr_document.draft[0]}" != "false"
-    return false
-  end
-
-  def edit_link_if_draft(document)
-    if is_draft(document)
-      return edit_curation_concerns_generic_work_path(document)
-    else
-      return curation_concerns_generic_work_path(document)
-    end
-  end
 end
