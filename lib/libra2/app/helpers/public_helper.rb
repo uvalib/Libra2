e6050be7ac40f8705a7a94e775bb91f9a11ee51f
Paul Rosen <paul@paulrosen.net>
2016-05-16 09:21:10 -0400

    First pass at redesigned public page.

--- lib/libra2/app/helpers/public_helper.rb
+++ lib/libra2/app/helpers/public_helper.rb
@@ -8,4 +8,12 @@ module PublicHelper
 		return "Persistent link will appear here after submission." if work.draft == "true"
 		return "http://dx.doi.org/#{work.identifier.gsub("doi:", "")}"
 	end
+
+	def display_rights(rights)
+		return "" if rights.blank?
+		rights = rights.join(" ") if rights.kind_of?(Array)
+		arr = rights.split("http")
+		return rights if arr.length != 2
+		return content_tag("a", arr[0], { href: "http" + arr[1] })
+	end
 end
