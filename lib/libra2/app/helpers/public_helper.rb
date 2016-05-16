cf35cb4cb544adc8ef919ac3aa9e32acf756b2a7
Paul Rosen <paul@paulrosen.net>
2016-05-16 10:49:02 -0400

    Some tweaks to the metadata on the public page.

--- lib/libra2/app/helpers/public_helper.rb
+++ lib/libra2/app/helpers/public_helper.rb
@@ -14,6 +14,14 @@ module PublicHelper
 		rights = rights.join(" ") if rights.kind_of?(Array)
 		arr = rights.split("http")
 		return rights if arr.length != 2
-		return content_tag("a", arr[0], { href: "http" + arr[1] })
+		return content_tag("a", arr[0], { href: "http" + arr[1], target: "_blank" })
+	end
+
+	def display_related_links(links)
+		a = []
+		links.each { |link|
+			a.push(content_tag(:a, link, { href: link, target: "_blank" }))
+		}
+		return raw(a.join(", "))
 	end
 end
