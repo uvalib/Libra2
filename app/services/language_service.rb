a0d3fb33f374e417a0f4838d6e8d4db75d8e9683
UVADave <dpg3k@virginia.edu>
2016-05-26 16:21:14 -0400

    Make language selectable from a dropdown

---
+++ app/services/language_service.rb
@@ -0,0 +1,16 @@
+module LanguageService
+  mattr_accessor :authority
+  self.authority = Qa::Authorities::Local.subauthority_for('languages')
+
+  def self.select_options
+    active_elements.map { |e| [e[:label], e[:id]] }
+  end
+
+  def self.label(id)
+    authority.find(id).fetch('term')
+  end
+
+  def self.active_elements
+    authority.all.select { |e| authority.find(e[:id])[:active] }
+  end
+end
