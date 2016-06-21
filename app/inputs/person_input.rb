71c9edc738ae8d5364e42b29f0cecbde1752047a
Paul Rosen <paul@paulrosen.net>
2016-06-21 11:50:54 -0400

    Change wording of advisor field help

--- app/inputs/person_input.rb
+++ app/inputs/person_input.rb
@@ -15,7 +15,7 @@ class PersonInput < MultiValueInput
 				end
 			end
 		end
-		return content_tag(:div, "Enter your chair as the first advisor.", { class: "field_help" }) + raw(ret)
+		return content_tag(:div, "Enter your committee chair as the first advisor.", { class: "field_help" }) + raw(ret)
 	end
 
 	private
