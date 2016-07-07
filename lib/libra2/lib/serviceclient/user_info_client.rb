ae870b9caaf63e12e0562cbb5c1d7189e9d3e1a8
UVADave <dpg3k@virginia.edu>
2016-07-07 13:14:13 -0400

    LIBRA-355: add healthcheck endpoint

--- lib/libra2/lib/serviceclient/user_info_client.rb
+++ lib/libra2/lib/serviceclient/user_info_client.rb
@@ -24,6 +24,15 @@ module ServiceClient
      private_class_method :new
 
      #
+     # check the health of the endpoint
+     #
+     def healthcheck
+       url = "#{self.url}/healthcheck"
+       status, _ = rest_get( url )
+       return( status )
+     end
+
+     #
      # get specified user information
      #
      def get_by_id( id )
