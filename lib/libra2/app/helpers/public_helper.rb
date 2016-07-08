127745b2c94d71b1ef29c8a6cc2186aacfcf98fe
UVADave <dpg3k@virginia.edu>
2016-07-08 11:03:10 -0400

    LIBRA-359: spacing on advisor name

--- lib/libra2/app/helpers/public_helper.rb
+++ lib/libra2/app/helpers/public_helper.rb
@@ -69,30 +69,11 @@ module PublicHelper
 			arr.push("") if arr.length == 4 # if the last item is empty, the split command will miss it.
 			# arr should be an array of [ computing_id, first_name, last_name, department, institution ]
 			if arr.length == 5
-				advisors.push("#{arr[2]}, #{arr[1]}, #{arr[3]}, #{arr[4]}")
+				advisors.push("#{arr[2].strip}, #{arr[1].strip}, #{arr[3].strip}, #{arr[4].strip}")
 			else
 				advisors.push(contributor) # this shouldn't happen, but perhaps it will if old data gets in there.
 			end
 		}
-		# first_name = work.contributor_first_name
-		# last_name = work.contributor_last_name
-		# department = work.contributor_department
-		# institution = work.contributor_institution
-		# advisors = []
-		# # these should all be the same length, but we're making sure anyway.
-		# if first_name.blank? || last_name.blank? || department.blank? || institution.blank?
-		# 	len = 0
-		# else
-		# 	len = 1000000
-		#
-		# 	len = first_name.length if first_name.length < len
-		# 	len = last_name.length if last_name.length < len
-		# 	len = department.length if department.length < len
-		# 	len = institution.length if institution.length < len
-		# end
-		# len.times { |i|
-		# 	advisors.push("#{last_name[i]}, #{first_name[i]}, #{department[i]}, #{institution[i]}")
-		# }
 		return raw(advisors.join("<br>"))
 	end
 
