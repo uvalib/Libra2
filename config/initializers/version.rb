7395d511e6c63df83ce43688b03265919becd917
UVADave <dpg3k@virginia.edu>
2016-08-01 14:12:59 -0400

    LIBRA-441: use the build tag as the version

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,1 +1,13 @@
-LIBRA_VERSION = 2.02
+#
+# get the version information from the build tag
+#
+def get_version
+  tag_pattern = "#{Rails.application.root}/buildtag.*"
+  files = Dir.glob( tag_pattern )
+  if files.length == 1
+    return File.basename( files[ 0 ] ).gsub( /buildtag./, '' )
+  end
+  return( 'unknown' )
+end
+
+BUILD_VERSION = get_version( )
--- lib/libra2/app/controllers/version_controller.rb
+++ lib/libra2/app/controllers/version_controller.rb
@@ -15,20 +15,8 @@ class VersionController < ApplicationController
   # # GET /version
   # # GET /version.json
   def index
-    version = get_version( )
-    response = VersionResponse.new( version )
+    response = VersionResponse.new( BUILD_VERSION )
     render json: response, :status => 200
-  end
-
-  private
-
-  def get_version
-    tag_pattern = "#{Rails.application.root}/buildtag.*"
-    files = Dir.glob( tag_pattern )
-    if files.length == 1
-       return File.basename( files[ 0 ] ).gsub( /buildtag./, '' )
-    end
-    return( 'unknown' )
   end
 
 end
