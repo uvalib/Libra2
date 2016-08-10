e1adb7a1715ecb303a4431a54a1f5979a40866ac
UVADave <dpg3k@virginia.edu>
2016-08-10 11:46:33 -0400

    LIBRA-467: render active links in additional fields on preview/public page

---
+++ app/renderers/curation_concerns/renderers/custom_public_attribute_renderer.rb
@@ -0,0 +1,33 @@
+module CurationConcerns
+  module Renderers
+    class CustomPublicAttributeRenderer < AttributeRenderer
+
+      def render
+        markup = ''
+        markup << %(<div class='document-row'>)
+        markup << label
+        markup << %(<br>)
+        Array(values).each do |value|
+          markup << attribute_value_to_html(value.to_s)
+        end
+        markup << %(</div>)
+        markup.html_safe
+      end
+
+      def label
+        content_tag(:span, @field, { class: "document-label" })
+      end
+
+      private
+
+      def attribute_value_to_html(value)
+          "<span document-value}>#{li_value(value)}</span>"
+      end
+
+      def li_value(value)
+        auto_link( ERB::Util.h(value), :html => { :target => '_blank' } )
+      end
+
+    end
+  end
+end
--- lib/libra2/app/helpers/public_helper.rb
+++ lib/libra2/app/helpers/public_helper.rb
@@ -24,50 +24,31 @@ module PublicHelper
 		end
 	end
 
-	def public_doi_link(work)
-		return "Persistent link will appear here after submission." if work.is_draft?
-		doi = work.permanent_url
-		return link_to(doi, doi)
-	end
-
-	def display_rights(rights)
-		return "" if rights.blank?
-		rights = rights.join(" ") if rights.kind_of?(Array)
-		arr = rights.split("http")
-		return rights if arr.length != 2
-		return content_tag("a", arr[0], { href: "http" + arr[1], target: "_blank" })
-	end
-
-	def display_related_links(links)
-		a = []
-		links.each { |link|
-			display = links.length > 1 ? raw("&bull; #{link}") : link
-			a.push(content_tag(:a, display, { href: link, target: "_blank" }))
-		}
-		return raw(a.join(""))
-	end
-
-	def display_keywords(work)
-		return "" if work.nil?
-		return work.keyword.join(", ")
-	end
-
 	def display_title(work)
-		return "Not Found" if work.nil?
+		return 'Not Found' if work.nil?
 		return work[:title][0]
 	end
 
-	def display_author(work)
-		return "" if work.nil?
+	def display_author( work )
+		author = construct_author( work )
+		return '' if author.blank?
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Author:", author ).render )
+	end
+
+	def construct_author( work )
+		return '' if work.nil?
 		return "#{work.author_last_name}, #{work.author_first_name}, #{work.department}, #{work.author_institution}"
 	end
 
 	def display_advisors(work)
+		return '' if work.nil?
+		return '' if work.contributor.blank?
+
 		contributors = work.contributor
 		advisors = []
 		contributors.each { |contributor|
 			arr = contributor.split("\n")
-			arr.push("") if arr.length == 4 # if the last item is empty, the split command will miss it.
+			arr.push('') if arr.length == 4 # if the last item is empty, the split command will miss it.
 			# arr should be an array of [ computing_id, first_name, last_name, department, institution ]
 			if arr.length == 5
 				advisors.push("#{arr[2].strip}, #{arr[1].strip}, #{arr[3].strip}, #{arr[4].strip}")
@@ -75,7 +56,68 @@ module PublicHelper
 				advisors.push(contributor) # this shouldn't happen, but perhaps it will if old data gets in there.
 			end
 		}
-		return raw(advisors.join("<br>"))
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Advisors:", advisors.join("<br>") ).render )
+	end
+
+	def display_description( description )
+		return '' if description.blank?
+		description = raw( description.gsub("\n", "<br>"))
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Abstract:", description ).render )
+	end
+
+	def display_degree( degree )
+		return '' if degree.blank?
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Degree:", degree ).render )
+	end
+
+	def display_keywords( work )
+		kw = construct_keywords( work )
+		return '' if kw.blank?
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Keywords:", kw ).render )
+	end
+
+	def construct_keywords( work )
+		return '' if work.nil?
+		return work.keyword.join( ', ')
+	end
+
+	def display_sponsoring_agency( sponsoring_agency )
+		return '' if sponsoring_agency.blank?
+		sa = sponsoring_agency.join( ' ')
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Sponsoring Agency:", sa ).render )
+	end
+
+	def display_related_links( links )
+		return '' if links.blank?
+		a = []
+		links.each { |link|
+			display = links.length > 1 ? raw("&bull; #{link}") : link
+			a.push( display )
+		}
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Related Links:", raw( a.join( '<br>' ) ) ).render )
+	end
+
+	def display_doi_link(work)
+		doi = "Persistent link will appear here after submission." if work.is_draft?
+		doi = work.permanent_url unless work.is_draft?
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Persistent Link:", doi ).render )
+	end
+
+	def display_notes(notes)
+    return '' if notes.blank?
+		notes = raw( notes.gsub("\n", "<br>"))
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Notes:", notes ).render )
+	end
+
+	def display_language( language )
+		return '' if language.blank?
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Language:", language ).render )
+	end
+
+	def display_rights(rights)
+		return '' if rights.blank?
+		rights = rights.join(' ') if rights.kind_of?(Array)
+		return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Rights:", rights ).render )
 	end
 
 	def display_array(value)
@@ -85,10 +127,4 @@ module PublicHelper
 		return value
 	end
 
-	def display_if_non_blank(label, value)
-		return "" if value.blank?
-		value = raw(value.gsub("\n", "<br>"))
-		row = raw(content_tag(:span, label, { class: "document-label" }) + content_tag(:span, value, { class: "document-value"}))
-		return content_tag(:div, row, { class: "document-row" })
-	end
 end
