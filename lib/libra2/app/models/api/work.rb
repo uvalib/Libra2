e91c8d2433f47ed37b2ef34f3924ebd9696b1b8b
UVADave <dpg3k@virginia.edu>
2016-08-12 09:52:00 -0400

    Add more fields to the API response

--- lib/libra2/app/models/api/work.rb
+++ lib/libra2/app/models/api/work.rb
@@ -4,35 +4,60 @@ class Work
 
   attr_accessor :id
   attr_accessor :author_email
-  attr_accessor :author_display_name
+  attr_accessor :author_first_name
+  attr_accessor :author_last_name
+
   attr_accessor :identifier
   attr_accessor :title
+  attr_accessor :abstract
   attr_accessor :create_date
   attr_accessor :modified_date
+
+  attr_accessor :creator_email
+  attr_accessor :embargo_state
+  attr_accessor :embargo_period
+  attr_accessor :embargo_end_date
+  attr_accessor :notes
+
   attr_accessor :status
+  attr_accessor :files
 
   def initialize( generic_work )
+
     @id = generic_work.id
     @author_email = generic_work.author_email
-    @author_display_name = 'bla bla bla'
+    @author_first_name = generic_work.author_first_name
+    @author_last_name = generic_work.author_last_name
+
     @identifier = generic_work.identifier
     @title = generic_work.title.join(' ')
+    @abstract = generic_work.description if generic_work.description.present?
 
-    tokens = generic_work.date_created.split( '/' )
-    @create_date = DateTime.new( tokens[0].to_i, tokens[1].to_i, tokens[2].to_i ).strftime( "%B %d, %Y" )
-    @modified_date = generic_work.date_modified.strftime( "%B %d, %Y" ) if generic_work.date_modified.present?
+    @create_date = generic_work.date_created
+    @modified_date = generic_work.date_modified if generic_work.date_modified.present?
+
+    @creator_email = generic_work.creator
+    @embargo_state = generic_work.embargo_state
+    @embargo_period = generic_work.embargo_period if generic_work.embargo_period.present?
+    @notes = generic_work.notes if generic_work.notes.present?
 
     if generic_work.is_draft?
-
       if generic_work.date_modified.present?
          @status = 'in-progress'
       else
         @status = 'pending'
       end
     else
-       @status = 'submitted'
+      @status = 'submitted'
+      @embargo_end_date = generic_work.embargo_end_date
     end
 
+    if generic_work.file_sets
+      @files = []
+      generic_work.file_sets.each do |file_set|
+        @files << { "#{file_set.id}" => { "label" => file_set.label, "title" => file_set.title[0] } }
+      end
+    end
   end
 
 end
