7846c8b7738208bfceccdb1ba548e30e5fe0c170
UVADave <dpg3k@virginia.edu>
2016-09-06 12:11:03 -0400

    LIBRA-506: disable links on edit page

--- lib/libra2/app/helpers/thesis_helper.rb
+++ lib/libra2/app/helpers/thesis_helper.rb
@@ -18,19 +18,19 @@ module ThesisHelper
     return content_tag(:tr, row, {})
   end
 
-    def requirement_class(requirements, type)
-      klass = "incomplete"
-      if requirements.present?
+  def requirement_class(requirements, type)
+     klass = "incomplete"
+     if requirements.present?
         klass = requirements[type] ? "complete" : "incomplete"
-      end
-        return klass
-    end
+     end
+     return klass
+  end
 
   def pending_file_override(requirements, type, pending_file)
     if @pending_file_test.present?
       return "complete"
     else
-        return requirement_class(requirements, type)
+      return requirement_class(requirements, type)
     end
   end
 
@@ -42,19 +42,28 @@ module ThesisHelper
     end
   end
 
-    def proof_is_readonly(requirements)
-      return !requirements[:files] || !requirements[:metadata]
+  def link_if( condition, title, url )
+    if condition == true
+      return raw( "<a href=\"#{url}\">#{title}</a>" )
+    else
+      return raw( title )
     end
+  end
 
-    def proof_tooltip(requirements)
-      if requirements[:files] && requirements[:metadata]
+  def proof_is_readonly(requirements)
+     return !requirements[:files] || !requirements[:metadata]
+  end
+
+  def proof_tooltip(requirements)
+     if requirements[:files] && requirements[:metadata]
         return "You have entered the required information. Click to continue the submission process."
-      elsif requirements[:files]
-          return "There are some required fields missing. Before continuing the submission process, click \"Edit\" to enter the missing data."
-      elsif requirements[:metadata]
-          return "You must upload at least one file to satisfy the requirements for submission. Click \"Edit\" to upload your files."
-      else
-          return "Before continuing the submission process, click \"Edit\" to enter all required fields and upload your files."
-      end
-    end
+     elsif requirements[:files]
+        return "There are some required fields missing. Before continuing the submission process, click \"Edit\" to enter the missing data."
+     elsif requirements[:metadata]
+        return "You must upload at least one file to satisfy the requirements for submission. Click \"Edit\" to upload your files."
+     else
+        return "Before continuing the submission process, click \"Edit\" to enter all required fields and upload your files."
+     end
+  end
+
 end
