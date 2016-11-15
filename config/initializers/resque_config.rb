f5c8cb058b13b6fe1b98324416ddfe394526d39d
UVADave <dpg3k@virginia.edu>
2016-11-15 12:15:11 -0500

    Modify resque config file so it can be used by the resque-web app

--- config/initializers/resque_config.rb
+++ config/initializers/resque_config.rb
@@ -1,8 +1,9 @@
+rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+config = YAML.load(ERB.new(IO.read(File.join(rails_root, 'config', 'redis.yml'))).result)[rails_env]
 Resque.redis = Redis.new( config.merge(thread_safe: true ) )
 
-Resque.inline = false #rails_env == 'development'
+Resque.inline = false
 Resque.redis.namespace = "libra2:#{rails_env}"
 Resque.logger.level = Logger::INFO
