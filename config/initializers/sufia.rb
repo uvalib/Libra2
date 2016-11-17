7341d6c83078e1f0bb2da1f4ad170b89450573f5
UVADave <dpg3k@virginia.edu>
2016-11-17 09:50:36 -0500

    Point tmp upload directory to the shared filesystem

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -98,6 +98,7 @@ Sufia.config do |config|
   # Temporary path to hold uploads before they are ingested into FCrepo.
   # This must be a lambda that returns a Pathname
   #  config.upload_path = ->() { Rails.root + 'tmp' + 'uploads' }
+  config.upload_path = ->() { Rails.root + 'hostfs' + 'uploads' + 'tmp' }
 
   # If browse-everything has been configured, load the configs.  Otherwise, set to nil.
   begin
