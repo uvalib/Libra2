2b0d6aef85cb3288a97666b4034e961b267738c7
UVADave <dpg3k@virginia.edu>
2017-02-07 10:02:26 -0500

    Back out LIBRA-684

--- lib/libra2/app/helpers/public_helper.rb
+++ lib/libra2/app/helpers/public_helper.rb
@@ -26,8 +26,9 @@ module PublicHelper
 
    def display_title(work)
       return 'Not Found' if work.nil?
-      title = CGI.unescapeHTML( String.new work[:title][0].to_s )
-      return raw( title )
+      return work[:title][0]
+      #title = CGI.unescapeHTML( String.new work[:title][0].to_s )
+      #return raw( title )
    end
 
    def display_author( work )
@@ -85,9 +86,11 @@ module PublicHelper
 
    def display_description( description )
       return '' if description.blank?
-      description = simple_format( description )
-      description = CGI.unescapeHTML( String.new description.to_s )
-      return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Abstract:", raw( description ) ).render )
+      description = raw( description.gsub("\n", "<br>"))
+      return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Abstract:", description ).render )
+      #description = simple_format( description )
+      #description = CGI.unescapeHTML( String.new description.to_s )
+      #return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Abstract:", raw( description ) ).render )
    end
 
    def display_degree( degree )
@@ -130,7 +133,8 @@ module PublicHelper
 
    def display_notes(notes)
     return '' if notes.blank?
-      notes = simple_format( notes )
+      notes = raw( notes.gsub("\n", "<br>"))
+      #notes = simple_format( notes )
       return( CurationConcerns::Renderers::CustomPublicAttributeRenderer.new("Notes:", notes ).render )
    end
 
