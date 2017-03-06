2e85038834cedd11ac8a4c430186ea9b07cafc8e
UVADave <dpg3k@virginia.edu>
2017-03-06 14:52:01 -0500

    Add logging to CreateDerivativesJob

--- vendor/gems/curation_concerns-1.3.3/app/jobs/create_derivatives_job.rb
+++ vendor/gems/curation_concerns-1.3.3/app/jobs/create_derivatives_job.rb
@@ -4,15 +4,23 @@ class CreateDerivativesJob < ActiveJob::Base
   # @param [FileSet] file_set
   # @param [String] file_id identifier for a Hydra::PCDM::File
   def perform(file_set, file_id)
+    puts "==> #{file_set.id}: enter CreateDerivativesJob:perform file_set.id = #{file_set.id}, file_id = #{file_id}"
     return if file_set.video? && !CurationConcerns.config.enable_ffmpeg
     filename = CurationConcerns::WorkingDirectory.find_or_retrieve(file_id, file_set.id)
+    puts "==> #{file_set.id}: filename = #{filename}"
 
+    puts "==> #{file_set.id}: calling file_set.create_derivatives()"
     file_set.create_derivatives(filename)
+    puts "==> #{file_set.id}: done"
 
     # Reload from Fedora and reindex for thumbnail and extracted text
+    puts "==> #{file_set.id}: calling file_set.reload()"
     file_set.reload
+    puts "==> #{file_set.id}: calling file_set.update_index()"
     file_set.update_index
+    puts "==> #{file_set.id}: calling file_set.parent.update_index()" if parent_needs_reindex?(file_set)
     file_set.parent.update_index if parent_needs_reindex?(file_set)
+    puts "==> #{file_set.id}: exit CreateDerivativesJob:perform"
   end
 
   # If this file_set is the thumbnail for the parent work,
