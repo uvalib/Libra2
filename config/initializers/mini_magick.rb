9fe94305ba5ca962dc7437286a8538d7365ff844
UVADave <dpg3k@virginia.edu>
2017-03-07 14:17:44 -0500

    Based on testing, change timeout to 30 minutes

--- config/initializers/mini_magick.rb
+++ config/initializers/mini_magick.rb
@@ -2,7 +2,7 @@ require 'mini_magick'
 
 MiniMagick.configure do |config|
   config.shell_api = "posix-spawn"
-  config.timeout = 600
+  config.timeout = 1800
   config.validate_on_create = false
   config.validate_on_write = false
   config.logger.level = Logger::DEBUG
