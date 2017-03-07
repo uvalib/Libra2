d088846114c07a4f3279e48a0760f2e8082c5507
UVADave <dpg3k@virginia.edu>
2017-03-07 13:27:14 -0500

    Set timeout to 10 minutes and disable validate on write

--- config/initializers/mini_magick.rb
+++ config/initializers/mini_magick.rb
@@ -2,6 +2,8 @@ require 'mini_magick'
 
 MiniMagick.configure do |config|
   config.shell_api = "posix-spawn"
-  config.timeout = 300
+  config.timeout = 600
   config.validate_on_create = false
+  config.validate_on_write = false
+  config.logger.level = Logger::DEBUG
 end
