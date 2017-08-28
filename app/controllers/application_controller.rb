6842db739df40c79805e2473faeab22ca08b2b50
UVADave <dpg3k@virginia.edu>
2017-08-28 09:54:41 -0400

    LIBRA-816: fix file upload, yay!!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,18 @@
 class ApplicationController < ActionController::Base
-  helper Openseadragon::OpenseadragonHelper
-  # Adds a few additional behaviors into the application controller 
+
+  # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
+  include CurationConcerns::ThemedLayoutController
+  with_themed_layout '1_column'
+
+  helper Openseadragon::OpenseadragonHelper
+  # Adds a few additional behaviors into the application controller
   include Hydra::Controller::ControllerBehavior
 
   # Adds CurationConcerns behaviors to the application controller.
-  include CurationConcerns::ApplicationControllerBehavior  
-  # Adds Sufia behaviors into the application controller 
+  include CurationConcerns::ApplicationControllerBehavior
+  # Adds Sufia behaviors into the application controller
   include Sufia::Controller
-
-  include CurationConcerns::ThemedLayoutController
-  layout 'sufia-one-column'
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
