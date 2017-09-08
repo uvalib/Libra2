a832f8b352bf862d286cbad26f0ab61d85cbd71a
UVADave <dpg3k@virginia.edu>
2017-09-08 10:05:55 -0400

    LIBRA-912: fix show page contributor labels

--- lib/libraetd/app/helpers/show_helper.rb
+++ lib/libraetd/app/helpers/show_helper.rb
@@ -34,6 +34,8 @@ module ShowHelper
   def show_line(presenter, field_name, label, required, options )
     tr = presenter.attribute_to_html(field_name, options.merge( { label: label } ) )
     if field_name == :contributor
+      # TODO - DPG: issue introduced in the sufia-7.3.1 upgrade; this is the hack workaround
+      tr = fix_contributor_label( tr )
       tr = show_contributor_required(tr)
     elsif required
       tr = show_wrap_required(tr)
@@ -44,24 +46,31 @@ module ShowHelper
     return tr
   end
 
-    def show_visibility_line(state, period)
-      th = content_tag(:th, "Visibility", {})
-      if state == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
-        state_str = t("libra.visibility.open.text")
-      elsif state == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
-        state_str = t("libra.visibility.embargo_engineering.text")
-        period = period[0] if period.kind_of?(Array)
-        period = GenericWork.displayable_embargo_period( period )
-        state_str = "#{state_str} until #{period} from submitting this thesis"
-      elsif state == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
-        state_str = t("libra.visibility.embargo.text")
-        period = period[0] if period.kind_of?(Array)
-        period = GenericWork.displayable_embargo_period( period )
-        state_str = "#{state_str} until #{period} from submitting this thesis"
-      end
-      state_el = content_tag(:li, state_str, { class: "attribute embargo_state" })
-      ul = content_tag(:ul, state_el, { class: "tabular"})
-      td = content_tag(:td, ul, {})
-      return content_tag(:tr, raw(th + td))
+  # TODO - DPG: issue introduced in the sufia-7.3.1 upgrade; this is the hack workaround
+  # try not to judge... we need to deploy monday and my brain is not working today
+  def fix_contributor_label( str )
+    return str.gsub( '<th>Contributors</th>', '<th>Advisors</th>' )
+  end
+
+  def show_visibility_line(state, period)
+    th = content_tag(:th, "Visibility", {})
+    if state == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+      state_str = t("libra.visibility.open.text")
+    elsif state == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+      state_str = t("libra.visibility.embargo_engineering.text")
+      period = period[0] if period.kind_of?(Array)
+      period = GenericWork.displayable_embargo_period( period )
+      state_str = "#{state_str} until #{period} from submitting this thesis"
+    elsif state == Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
+      state_str = t("libra.visibility.embargo.text")
+      period = period[0] if period.kind_of?(Array)
+      period = GenericWork.displayable_embargo_period( period )
+      state_str = "#{state_str} until #{period} from submitting this thesis"
     end
+    state_el = content_tag(:li, state_str, { class: "attribute embargo_state" })
+    ul = content_tag(:ul, state_el, { class: "tabular"})
+    td = content_tag(:td, ul, {})
+    return content_tag(:tr, raw(th + td))
+  end
+
 end
