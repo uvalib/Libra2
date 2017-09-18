2014c2ee15b7ce7d9619fb048db7cd1b3d333595
UVADave <dpg3k@virginia.edu>
2017-09-18 15:48:51 -0400

    LIBRA-931: pull downloaded content from Fedora, not file system

--- lib/libraetd/app/controllers/api_v1_downloads_controller.rb
+++ lib/libraetd/app/controllers/api_v1_downloads_controller.rb
@@ -6,15 +6,15 @@ class APIV1DownloadsController < APIBaseController
   def get_content
     fileset = get_the_fileset
     if fileset.nil? == false
-       filename = "#{CurationConcerns.config.working_path}/#{source_name_from_fileset( fileset )}"
-#       puts "==> content #{filename}"
-       if File.exist?( filename )
-          mimetype = MIME::Types.type_for( filename ).first.content_type
-          send_file filename, :type => mimetype, :disposition => "attachment; filename=#{destination_name_from_fileset( fileset )}"
+       file = fileset.original_file
+       if file.nil? == false
+          send_file_contents( file, fileset.title[ 0 ] )
        else
+          puts "==> ERROR: cannot determine original file for fileset #{fileset.id}"
           render_nothing
        end
     else
+       puts "==> ERROR: cannot locate fileset #{params[:id]}"
        render_nothing
     end
 
@@ -29,13 +29,14 @@ class APIV1DownloadsController < APIBaseController
     fileset = get_the_fileset
     if fileset.nil? == false
        filename = "#{CurationConcerns.config.derivatives_path}/#{thumbnail_from_fileset( fileset )}"
-#       puts "==> thumbnail #{filename}"
        if File.exist?( filename )
           send_file filename, :type => 'image/jpeg', :disposition => 'inline'
        else
+          puts "==> ERROR: cannot locate thumbnail for fileset #{fileset.id} (#{filename})"
           render_nothing
        end
     else
+       puts "==> ERROR: cannot locate fileset #{params[:id]}"
        render_nothing
     end
 
@@ -47,14 +48,6 @@ class APIV1DownloadsController < APIBaseController
     render :nothing => true, :type => 'text/plain', :status => :not_found
   end
 
-  def source_name_from_fileset( fileset )
-    return "#{dirname_from_fileset( fileset )}/#{fileset.label}"
-  end
-
-  def destination_name_from_fileset( fileset )
-    return "#{fileset.title[ 0 ]}"
-  end
-
   def thumbnail_from_fileset( fileset )
     return "#{dirname_from_fileset( fileset )}/#{fileset.id[8]}-thumbnail.jpeg"
   end
@@ -62,6 +55,35 @@ class APIV1DownloadsController < APIBaseController
   def dirname_from_fileset( fileset )
     id = fileset.id
     return "#{id[0]}#{id[1]}/#{id[2]}#{id[3]}/#{id[4]}#{id[5]}/#{id[6]}#{id[7]}"
+  end
+
+  #
+  # shamelessly taken from Hydra::Controller::DownloadBehavior
+  #
+
+  def send_file_contents( file, filename )
+    response.status = 200
+    prepare_file_headers( file, filename )
+    stream_body( file.stream )
+  end
+
+  def prepare_file_headers( file, filename )
+    send_file_headers! content_options( file, filename )
+    response.headers['Content-Type'] = file.mime_type
+    response.headers['Content-Length'] ||= file.size.to_s
+    response.content_type = file.mime_type
+  end
+
+  def stream_body(iostream)
+    iostream.each do |in_buff|
+      response.stream.write in_buff
+    end
+  ensure
+    response.stream.close
+  end
+
+  def content_options( file, filename )
+    { type: file.mime_type, filename: filename }
   end
 
 end
