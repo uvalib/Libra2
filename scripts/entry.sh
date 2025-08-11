c0311b5d5d0e8437f2a8133cc2da2e9082907ea4
Dave G <dpg3k@virginia.edu>
2025-08-11 13:47:21 -0400

    Disable SIS and optional import services

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -4,10 +4,10 @@
 rm -f $APP_HOME/tmp/pids/server.pid > /dev/null 2>&1
 
 # run the optional deposit importer process
-scripts/optional_deposit_import.sh &
+#scripts/optional_deposit_import.sh &
 
 # run the SIS deposit importer process
-scripts/sis_deposit_import.sh &
+#scripts/sis_deposit_import.sh &
 
 # run the sitemap generator process
 scripts/sitemap_generator.sh &
